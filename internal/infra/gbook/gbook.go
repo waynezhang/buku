178ba22f4c484bf2731f01c513eed67d4f969375
Linghua Zhang <linghua.zhang@me.com>
2024-06-11 21:54:05 +0900

    feat: add info link for google books result

--- internal/infra/gbook/gbook.go
+++ internal/infra/gbook/gbook.go
@@ -10,44 +10,42 @@ import (
 )
 
 type Book struct {
-	Id     int
-	Title  string
-	Author string
-	ISBN   string
+	Id       int
+	Title    string
+	Author   string
+	ISBN     string
+	InfoLink string
 }
 
-type Volume struct {
-	Title         string   `json:"title"`
-	Authors       []string `json:"authors"`
-	PublishedDate string   `json:"publishedDate"`
-	PageCount     int      `json:"pageCount"`
-	Categories    []string `json:"categories"`
-	ImageLinks    struct {
+type volume struct {
+	Title      string   `json:"title"`
+	Authors    []string `json:"authors"`
+	ImageLinks struct {
 		Thumbnail string `json:"thumbnail"`
 	}
 	IndustryIdentifiers []struct {
 		Type       string `json:"type"`
 		Identifier string `json:"identifier"`
 	}
-	Language string `json:"language"`
+	InfoLink string `json:"infoLink"`
 }
 
 type item struct {
-	Volume Volume `json:"volumeInfo"`
+	Volume volume `json:"volumeInfo"`
 }
 
 type response struct {
 	Items []item `json:"items"`
 }
 
-func (v *Volume) FirstAuthor() string {
+func (v *volume) FirstAuthor() string {
 	if len(v.Authors) > 0 {
 		return v.Authors[0]
 	}
 	return ""
 }
 
-func (v *Volume) ISBN() string {
+func (v *volume) ISBN() string {
 	for _, ii := range v.IndustryIdentifiers {
 		if ii.Type == "ISBN_13" {
 			return ii.Identifier
@@ -74,10 +72,11 @@ func Search(query string, maxResults string) []Book {
 	books := []Book{}
 	for i, item := range results.Items {
 		books = append(books, Book{
-			Id:     i,
-			Title:  item.Volume.Title,
-			Author: item.Volume.FirstAuthor(),
-			ISBN:   item.Volume.ISBN(),
+			Id:       i,
+			Title:    item.Volume.Title,
+			Author:   item.Volume.FirstAuthor(),
+			ISBN:     item.Volume.ISBN(),
+			InfoLink: item.Volume.InfoLink,
 		})
 	}
 
